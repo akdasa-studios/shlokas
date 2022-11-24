75a5fddd3fa469e57c9584d5c106bd0b8b19228a
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-11-24 11:56:10 +0000

    fix test

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -4,11 +4,7 @@ test('homepage has title and links to intro page', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  if (Math.random() * 101 < 50) {
-    await expect(page).toHaveTitle(/Shlokas FAILURE/)
-  } else {
-    await expect(page).toHaveTitle(/Shlokas/)
-  }
+  await expect(page).toHaveTitle(/Shlokas/)
 
   // // create a locator
   // const getStarted = page.getByRole('link', { name: 'Get started' });
