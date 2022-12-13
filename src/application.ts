1d32d6deecfa7856d1068483330a4c01b4275838
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-12-13 18:10:50 +0000

    feat: more verses

--- src/application.ts
+++ src/application.ts
@@ -31,6 +31,86 @@ versesRepo.save(new VerseBuilder()
   ))
   .build().value)
 
+versesRepo.save(new VerseBuilder()
+  .withNumber(new VerseNumber('BG 2.12'))
+  .withText(new Text([
+    'na tv evāhaṁ jātu nāsaṁ',
+    'na tvaṁ neme janādhipāḥ',
+    'na caiva na bhaviṣyāmaḥ',
+    'sarve vayam ataḥ param'
+  ]))
+  .withTranslation(new Translation(
+    'Never was there a time when I did not exist, nor you, nor all these kings; nor in the future shall any of us cease to be.'
+  ))
+  .build().value)
+
+versesRepo.save(new VerseBuilder()
+  .withNumber(new VerseNumber('BG 2.13'))
+  .withText(new Text([
+    'dehino ’smin yathā dehe',
+    'kaumāraṁ yauvanaṁ jarā',
+    'tathā dehāntara-prāptir',
+    'dhīras tatra na muhyati'
+  ]))
+  .withTranslation(new Translation(
+    'As the embodied soul continuously passes, in this body, from boyhood to youth to old age, the soul similarly passes into another body at death. A sober person is not bewildered by such a change.'
+  ))
+  .build().value)
+
+versesRepo.save(new VerseBuilder()
+  .withNumber(new VerseNumber('BG 2.20'))
+  .withText(new Text([
+    'na jāyate mriyate vā kadācin',
+    'nāyaṁ bhūtvā bhavitā vā na bhūyaḥ',
+    'ajo nityaḥ śāśvato ’yaṁ purāṇo',
+    'na hanyate hanyamāne śarīre',
+  ]))
+  .withTranslation(new Translation(
+    'For the soul there is neither birth nor death at any time. He has not come into being, does not come into being, and will not come into being. He is unborn, eternal, ever-existing and primeval. He is not slain when the body is slain.'
+  ))
+  .build().value)
+
+versesRepo.save(new VerseBuilder()
+  .withNumber(new VerseNumber('BG 2.22'))
+  .withText(new Text([
+    'vāsāṁsi jīrṇāni yathā vihāya',
+    'navāni gṛhṇāti naro ’parāṇi',
+    'tathā śarīrāṇi vihāya jīrṇāny',
+    'anyāni saṁyāti navāni dehī'
+  ]))
+  .withTranslation(new Translation(
+    'As a person puts on new garments, giving up old ones, the soul similarly accepts new material bodies, giving up the old and useless ones.'
+  ))
+  .build().value)
+
+
+versesRepo.save(new VerseBuilder()
+  .withNumber(new VerseNumber('BG 2.41'))
+  .withText(new Text([
+    'vyavasāyātmikā buddhir',
+    'ekeha kuru-nandana',
+    'bahu-śākhā hy anantāś ca',
+    'buddhayo ’vyavasāyinām',
+  ]))
+  .withTranslation(new Translation(
+    'Those who are on this path are resolute in purpose, and their aim is one. O beloved child of the Kurus, the intelligence of those who are irresolute is many-branched.'
+  ))
+  .build().value)
+
+versesRepo.save(new VerseBuilder()
+  .withNumber(new VerseNumber('BG 2.44'))
+  .withText(new Text([
+    'bhogaiśvarya-prasaktānāṁ',
+    'tayāpahṛta-cetasām',
+    'vyavasāyātmikā buddhiḥ',
+    'samādhau na vidhīyate',
+  ]))
+  .withTranslation(new Translation(
+    'In the minds of those who are too attached to sense enjoyment and material opulence, and who are bewildered by such things, the resolute determination for devotional service to the Supreme Lord does not take place.'
+  ))
+  .build().value)
+
+
 const app = new Application(versesRepo)
 
 export function useApp() : Application { return app }
