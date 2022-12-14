c73e3a3ba51b3c1e58e7988f4b0cf1b89a28ef4f
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-12-14 18:51:22 +0000

    fix: css

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
+
+console.log(window.location.hostname)
+if (window.location.hostname === 'shlokas-staging.netlify.app') {
+  console.log("loading custom.css")
+  require('./theme/custom.css')
+}
+
 export default defineComponent({
   name: 'App',
   components: {
