6e301d663fa1a1b4456ad2d07f70ec9470696b3c
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-12-14 19:34:38 +0000

    feat: tricky way to make demo stang looks great

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,19 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
+try {
+  if (window.location.search.includes("demo")) {
+    var css = `:root {
+      --ion-safe-area-top: 20px;
+      --ion-safe-area-bottom: 22px;
+    }`
+    var head = document.head || document.getElementsByTagName('head')[0]
+    var style = document.createElement('style')
+    head.appendChild(style);
+    style.appendChild(document.createTextNode(css));
+  }
+} catch(e) { console.log(e) }
+
 export default defineComponent({
   name: 'App',
   components: {
