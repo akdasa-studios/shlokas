dbee421cb880101724c474b86303a719304e6eb9
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-12-14 18:59:46 +0000

    rollback

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,6 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
-
-console.log(window.location.hostname)
-if (window.location.search.includes("demo")) {
-  console.log("loading custom.css")
-  require('./theme/custom.css')
-}
-
 export default defineComponent({
   name: 'App',
   components: {
