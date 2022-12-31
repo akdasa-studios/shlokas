a34faadcdee8142848db01c4741472e3a32b788b
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-12-31 12:52:59 +0100

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -102,11 +102,11 @@ const config: PlaywrightTestConfig = {
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   outputDir: 'test-results/',
 
-  // /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run serve',
-  //   port: 8080,
-  // },
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run serve',
+    port: 8080,
+  },
 }
 
 export default config
