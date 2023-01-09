6af95b4933dfb6935ec8decde7df98c179d44bc8
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-01-09 16:31:35 +0100

    test

--- tests/e2e/library/AddToInbox.spec.ts
+++ tests/e2e/library/AddToInbox.spec.ts
@@ -1,11 +1,44 @@
+import fs from 'fs'
 import { test } from '@playwright/test'
+import v8toIstanbul  from 'v8-to-istanbul'
 import { Application } from './pages/Application'
+
 
 let app: Application
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page, browserName }) => {
+  if (browserName === 'chromium') {
+    await page.coverage.startJSCoverage()
+  }
+
   app = new Application(page)
   await app.library.open()
+})
+
+test.afterEach(async ({ page, browserName }) => {
+  if (browserName !== 'chromium') { return }
+
+  const coverage = await page.coverage.stopJSCoverage()
+  for (const entry of coverage) {
+    const converter = v8toIstanbul('', 0, { source: entry.source || '' })
+    await converter.load()
+    converter.applyCoverage(entry.functions)
+    const report = converter.toIstanbul() as any
+
+    const key = Object.keys(report)[0]
+    if (!key) { continue }
+    if (key.includes('node_modules')) { continue }
+    const normalized_key = key.split('?')[0].replace('/Users/akdasa/Projects/akdasa-studios/shlokas/@akdasa-studios/shlokas/', '')
+    const data = { [normalized_key]: report[key] }
+    data[normalized_key]["path"] = normalized_key
+
+    const filename = Math.random().toString(36).substr(2, 5)
+
+    fs.writeFile(".nyc_output/coverage_" + filename + ".json", JSON.stringify(data), () => {
+      // pass
+    })
+    // console.log())
+  }
 })
 
 test.describe('Add verse', () => {
