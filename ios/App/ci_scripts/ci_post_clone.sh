852b938bc5bc0283bec1560eeb6211a03ce6fdc4
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-02-04 10:30:23 +0100

    fix?

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,12 @@
 brew install cocoapods
 brew install node@18
 brew link node@18
-pod install
 
-# build web project
 cd $CI_WORKSPACE
 npm install
+
+cd ./ios/App
+pod install
+
+cd $CI_WORKSPACE
 npm run build
