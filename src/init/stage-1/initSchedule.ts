fa149e9a16ff70fa7d93e0ce936ab54e89cad968
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-02-06 13:59:27 +0100

    Review deck is not updated on the next date Fixes #142

--- src/init/stage-1/initSchedule.ts
+++ src/init/stage-1/initSchedule.ts
@@ -1,9 +1,4 @@
 import { App } from '@capacitor/app'
-import { Capacitor } from '@capacitor/core'
-import { BackgroundTask } from '@capawesome/capacitor-background-task'
-import { AUTH_HOST } from '@/app/Env'
-import { useAccountStore } from '@/app/settings'
-import { AuthService } from '@/services/AuthService'
 import { InitArgs } from '../initialization'
 
 export async function initSchedule(
