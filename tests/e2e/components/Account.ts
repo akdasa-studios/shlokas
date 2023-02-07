5d386faf9fd732f2125cd1ffdcfd69839d96b4b9
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-02-07 14:40:29 +0100

    fix

--- tests/e2e/components/Account.ts
+++ tests/e2e/components/Account.ts
@@ -3,7 +3,7 @@ import { Page } from "@playwright/test"
 export class Account {
   constructor(private readonly page: Page) {}
 
-  get signUpViaEmail()   { return this.page.getByTestId("signUp") }
+  get signUpViaEmail() { return this.page.getByRole('button', { name: 'Sign Up'})  }
   get name()     { return this.page.getByRole('textbox', { name: 'Name' }) }
   get login()    { return this.page.getByRole('textbox', { name: 'EMail'}) }
   get password() { return this.page.getByRole('textbox', { name: 'Password' }) }
