498fb8e1df49209ed1d5dffa07c0b4d217ea3d58
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-02-18 14:48:53 +0100

    fix: sentry app version (#155)
    Fixes #154

--- src/init/stage-0/initSentry.ts
+++ src/init/stage-0/initSentry.ts
@@ -1,23 +1,29 @@
 import * as Sentry from '@sentry/capacitor'
 import { BrowserTracing } from '@sentry/tracing'
 import * as SentrySibling from '@sentry/vue'
+import { App } from '@capacitor/app'
 import { InitArgs } from '../initialization'
 
 /**
  * Initialize the sentry plugin to track errors
  */
 export async function initSentry({ vue }: InitArgs) {
-  if (process.env.NODE_ENV === 'development') {
-    return
+  if (process.env.NODE_ENV === 'development') { return }
+
+  let version = 'unknown'
+  try {
+    const info = await App.getInfo()
+    version = info.version
+  } catch (e) {
+    console.error('Failed to get app version', e)
   }
 
-  const VERSION = process.env.npm_package_version
   const DSN = 'https://e09ab355192945fb87bc01882eb62578@o257342.ingest.sentry.io/4504486578225152'
 
   Sentry.init({
     app: vue,
     dsn: DSN,
-    release: `shlokas@${VERSION}`,
+    release: `shlokas@${version}`,
     dist: '1',
     tracesSampleRate: 1.0,
     integrations: [
