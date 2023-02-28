ab40403bc1420cde890e4682e5e6059127fada92
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-02-28 19:11:02 +0100

    feat: refactoring

--- src/app/decks/shared/VerseTextLines.vue
+++ src/app/decks/shared/VerseTextLines.vue
@@ -1,42 +1,24 @@
 <template>
-  <div class="root">
+  <div>
     <DarkImage
-      v-if="props.uri && localSvgUri"
-      :src="localSvgUri"
+      v-if="props.uri && imageUri"
+      :src="imageUri"
       :is-dark="isDark"
       mode="invert"
     />
 
-    <svg
+    <SVGTextLines
       v-else
-      ref="svg"
-      width="100%"
-      height="100%"
-    >
-      <text
-        v-for="line, idx in props.lines"
-        :key="line"
-        :y="getYPos(idx)"
-        x="50%"
-        dominant-baseline="middle"
-        text-anchor="middle"
-        class="line"
-      >
-        {{ line }}
-      </text>
-    </svg>
-
-    <ion-spinner
-      v-if="downloadService.isDownloading.value"
-      class="spinner"
+      :lines="props.lines"
+      line-class="line"
+      line-height="20"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, defineProps, ref } from 'vue'
-import { IonSpinner } from '@ionic/vue'
-import { DarkImage } from '@akdasa-studios/shlokas-uikit'
+import { DarkImage, SVGTextLines } from '@akdasa-studios/shlokas-uikit'
 import { usePreferredDark } from '@vueuse/core'
 import { useDownloadService } from '@/app/shared'
 
@@ -48,45 +30,16 @@ const props = defineProps<{
 const downloadService = useDownloadService()
 const isDark = usePreferredDark()
 
-const svg = ref()
-const FONT_SIZE = 15
-const LINE_HEIGHT = 1.1
-const MARGIN = 10
-const localSvgUri = ref('')
-
-function getYPos(idx: number): number {
-  return (MARGIN/2) + (FONT_SIZE * LINE_HEIGHT * (idx+1)) - (FONT_SIZE/2)
-}
-
-function recalculateSize() {
-  var  bbox = svg.value.getBBox()
-  svg.value.setAttribute('viewBox', `0 0 ${bbox.width+MARGIN} ${bbox.height+MARGIN}`)
-  if (bbox.width === 0) { setTimeout(recalculateSize, 25) }
-}
+const imageUri = ref('')
 
 onMounted(async () => {
-  if (props.uri) {
-    localSvgUri.value = await downloadService.download(props.uri)
-  } else {
-    setTimeout(() => { recalculateSize() }, 1)
-  }
+  if (props.uri) { imageUri.value = await downloadService.download(props.uri) }
 })
 </script>
 
-<style scoped>
-.root {
-  width: 100%;
-  position: relative;
-}
-.line {
+<style scoped lang="scss">
+/deep/ .line {
+  font-family: Georgia;
   fill: var(--ion-color-light-contrast);
-  font-size: 15px;
-  font-style: italic;
-}
-
-.spinner {
-  position:absolute;
-  bottom: 0%;
-  right: 0%;
 }
 </style>
