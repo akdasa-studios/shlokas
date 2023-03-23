4625fbba4bed5baf1b6c9664768bb3ef76e85870
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-03-23 13:02:42 +0100

    moar

--- src/app/decks/inbox/index.ts
+++ src/app/decks/inbox/index.ts
@@ -2,8 +2,8 @@
 export { default as InboxDeckPage } from './pages/InboxDeckPage.vue'
 export { default as InboxDeckEmpty } from './components/InboxDeckEmpty.vue'
 export { default as StackedFlipCardsDeck } from './components/StackedFlipCardsDeck.vue'
-export { default as CircleProgress } from './components/cards/CircleProgress.vue'
-export * from './components/cards'
+export { default as CircleProgress } from './components/CircleProgress.vue'
+export * from './components'
 
 // routes:
 export * from './routes/inboxDeck'
