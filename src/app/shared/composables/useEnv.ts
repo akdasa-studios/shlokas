ab1925b720c4ca25850f3b6ee62c39a7b288b811
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-04-12 19:01:33 +0200

    fix sukablya

--- src/app/shared/composables/useEnv.ts
+++ src/app/shared/composables/useEnv.ts
@@ -5,10 +5,6 @@ export function useEnv() {
   const PROTOCOL = import.meta.env.VITE_PROTOCOL
   const HOST     = import.meta.env.VITE_HOST
 
-  console.log(MODE)
-  console.log(PROTOCOL)
-  console.log(HOST)
-
   function getContentUrl(fileName: string): string {
     return `${PROTOCOL}://${HOST}/content/${fileName}`
   }
