bccc648cab74f85ba400ee2830aea7f998f7bbcc
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-13 12:56:15 +0200

    fix: xcode ci/cd pipeline

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -5,7 +5,7 @@ brew link node@18
 
 cd "$CI_WORKSPACE"
 npm install
-npm run build
+npm run build:prod
 npx capacitor-set-version set:ios -v $(npm pkg get version) -b $CI_BUILD_NUMBER
 npx cap sync
 
