5d26ffb4403ed4d5c8b73cc1bc8eb51622c1161a
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-18 10:54:35 +0200

    feat: google auth (#217)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,15 @@ const config: CapacitorConfig = {
   appName: 'shlokas',
   webDir: 'dist',
   bundledWebRuntime: false,
-  // server: {
-  //   url: 'http://192.168.0.13:8100',
-  // }
+  plugins: {
+    GoogleAuth: {
+      'scopes': ['profile', 'email'],
+      'serverClientId': '879891505939-bs3v8sm9mu1ti65eu81f3b8idtl8bakm.apps.googleusercontent.com',
+      'iosClientId': '879891505939-uojhetilbf9meih9veisruc2lfu8im26.apps.googleusercontent.com',
+      'androidClientId': '879891505939-ff7afgp92l52270ojarcqa594s3ev0ss.apps.googleusercontent.com',
+      'forceCodeForRefreshToken': true
+    }
+  }
 }
 
 export default config
--- src/init/infrastructure/initAuth.ts
+++ src/init/infrastructure/initAuth.ts
@@ -1,9 +1,10 @@
 import { useAuthentication } from '@/app/shared'
-import { EmailAuthenticationStrategy, AppleAuthenticationStrategy } from '@/services/auth/strategies'
+import { EmailAuthenticationStrategy, AppleAuthenticationStrategy, GoogleAuthenticationStrategy } from '@/services/auth/strategies'
 
 
 export async function initAuth() {
   const auth = useAuthentication()
   auth.registerStrategy('email', new EmailAuthenticationStrategy())
   auth.registerStrategy('apple', new AppleAuthenticationStrategy())
+  auth.registerStrategy('google', new GoogleAuthenticationStrategy())
 }
