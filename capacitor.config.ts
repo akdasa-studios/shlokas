0e0e1e6792866aafc1282a47a6b81982dfa000b4
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-19 18:29:57 +0200

    fix: use web client ID for android. Don't ask me why...

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -10,7 +10,8 @@ const config: CapacitorConfig = {
       'scopes': ['profile', 'email'],
       'serverClientId': '879891505939-bs3v8sm9mu1ti65eu81f3b8idtl8bakm.apps.googleusercontent.com',
       'iosClientId': '879891505939-uojhetilbf9meih9veisruc2lfu8im26.apps.googleusercontent.com',
-      'androidClientId': '879891505939-ff7afgp92l52270ojarcqa594s3ev0ss.apps.googleusercontent.com',
+      // We use WEB client ID here. Don't ask me why...
+      'androidClientId': '879891505939-bs3v8sm9mu1ti65eu81f3b8idtl8bakm.apps.googleusercontent.com',
       'forceCodeForRefreshToken': true
     }
   }
