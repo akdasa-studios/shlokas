1a5441cfc6cfecdd4bd487821eac9a2b67c95a70
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-19 16:58:11 +0200

    fix: follow instructions

--- android/app/src/main/java/com/akdasa/shlokas/MainActivity.java
+++ android/app/src/main/java/com/akdasa/shlokas/MainActivity.java
@@ -1,5 +1,14 @@
 package com.akdasa.shlokas;
 
+import android.os.Bundle;
+
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.registerPlugin(GoogleAuth.class);
+    }
+}
