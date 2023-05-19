32f19ff27191ebd0a63a6c16a091ea62b029884d
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-19 13:04:11 +0200

    feat: add sign buttons to account page

--- src/app/settings/pages/AccountPage.vue
+++ src/app/settings/pages/AccountPage.vue
@@ -14,17 +14,41 @@
         {{ $t('account.welcomeBack') }}
       </h1>
 
-      <ion-button
+      <IonButton
         v-if="!isAuthenticated"
         expand="block"
-        router-link="/home/settings/account/email"
+        color="dark"
+        @click="onSignIn('apple')"
+      >
+        <ion-icon
+          slot="start"
+          :icon="logoApple"
+        />
+        {{ $t("welcome.login.withAppleId") }}
+      </IonButton>
+      <IonButton
+        v-if="!isAuthenticated"
+        expand="block"
+        color="danger"
+        @click="onSignIn('google')"
+      >
+        <ion-icon
+          slot="start"
+          :icon="logoGoogle"
+        />
+        {{ $t("welcome.login.withGoogle") }}
+      </IonButton>
+      <IonButton
+        v-if="!isAuthenticated"
+        expand="block"
+        @click="onEmailSignIn"
       >
         <ion-icon
           slot="start"
           :icon="mail"
         />
-        {{ $t('account.logIn') }}
-      </ion-button>
+        {{ $t("welcome.login.withEmail") }}
+      </IonButton>
 
       <ion-button
         v-if="isAuthenticated"
@@ -50,14 +74,14 @@
 <script lang="ts" setup>
 import {
   IonBackButton, IonButton, IonButtons, IonContent,
-  IonHeader, IonPage, IonTitle, IonToolbar, IonLoading, IonIcon
+  IonHeader, IonPage, IonTitle, IonToolbar, IonLoading, IonIcon, useIonRouter, alertController
 } from '@ionic/vue'
-import { mail } from 'ionicons/icons'
+import { mail, logoApple, logoGoogle } from 'ionicons/icons'
 import { computed, inject, ref } from 'vue'
 import { EventEmitter2 } from 'eventemitter2'
 import { Context, TimeMachine } from '@akdasa-studios/shlokas-core'
 import { createRepositories } from '@/app/utils/sync'
-import { useApplication, useEnv } from '@/app/shared'
+import { go, useApplication, useAuthentication, useEnv } from '@/app/shared'
 import { useSettingsStore } from '@/app/settings'
 
 /* -------------------------------------------------------------------------- */
@@ -69,6 +93,8 @@ const emitter = inject('emitter') as EventEmitter2
 const application = useApplication()
 const settings = useSettingsStore()
 const env = useEnv()
+const auth = useAuthentication()
+const router = useIonRouter()
 
 
 /* -------------------------------------------------------------------------- */
@@ -91,5 +117,24 @@ async function onSync() {
   await application.instance().sync(new Context('sync', new TimeMachine(), remoteRepos))
   emitter.emit('syncCompleted')
   setTimeout(() => inProgress.value = false, 2500)
+}
+
+
+async function onSignIn(strategy: string) {
+  try {
+    await auth.authenticate(strategy)
+  } catch (e) {
+    const alert = await alertController.create({
+      header: 'Error',
+      subHeader: `Unable to sign in with ${strategy}.`,
+      message: ((e as Error).message),
+      buttons: ['OK'],
+    })
+    await alert.present()
+  }
+}
+
+async function onEmailSignIn() {
+  router.push(go('welcome-email'))
 }
 </script>
