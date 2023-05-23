b11257f7462f46ae708cfe215b64fef32fe86d1e
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-23 16:28:08 +0200

    fix

--- src/init/infrastructure/initLogging.ts
+++ src/init/infrastructure/initLogging.ts
@@ -11,20 +11,27 @@ export async function initLogging() {
 
 class ConsoleLogTransport implements LogTransport {
   log(record: LogRecord): void {
-    const context = `[${record.context}]`
-    const data = record.data
-    const styles = {
-      context: 'color: #9E9E9E; font-weight: bold',
-    }
 
-    if (record.level === LogLevel.DEBUG) {
-      console.debug('%c'+context, styles.context, record.message, data)
-    } else if (record.level === LogLevel.INFO) {
-      console.info('%c'+context, record.message, data)
-    } else if (record.level === LogLevel.WARN) {
-      console.warn('%c'+context, record.message, data)
-    } else if (record.level === LogLevel.ERROR || record.level === LogLevel.FATAL) {
-      console.error('%c'+context, record.message, data)
+    if (record.type === 'log') {
+      const context = `[${record.context}]`
+      const data = record.data
+      const styles = {
+        context: 'color: #9E9E9E; font-weight: bold',
+      }
+
+      if (record.level === LogLevel.DEBUG) {
+        console.debug('%c' + context, styles.context, record.message, data)
+      } else if (record.level === LogLevel.INFO) {
+        console.info('%c' + context, record.message, data)
+      } else if (record.level === LogLevel.WARN) {
+        console.warn('%c' + context, record.message, data)
+      } else if (record.level === LogLevel.ERROR || record.level === LogLevel.FATAL) {
+        console.error('%c' + context, record.message, data)
+      }
+    } else if (record.type === 'start-group') {
+      console.group(record.label)
+    } else if (record.type === 'end-group') {
+      console.groupEnd()
     }
   }
 }
