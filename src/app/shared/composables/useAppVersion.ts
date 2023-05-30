cfec5eb3ec4434f648f7cf54b2914f07be37f83a
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-30 22:49:29 +0200

    fix: useAppVersion for web

--- src/app/shared/composables/useAppVersion.ts
+++ src/app/shared/composables/useAppVersion.ts
@@ -1,7 +1,12 @@
 import { Deploy } from 'cordova-plugin-ionic'
 import { App } from '@capacitor/app'
+import { Capacitor } from '@capacitor/core'
 
 export async function useAppVersion() {
+  if (Capacitor.getPlatform() === 'web') {
+    return 'web'
+  }
+
   let appInfo = { version: '??', build: '??' }
   try {
     appInfo = await App.getInfo()
