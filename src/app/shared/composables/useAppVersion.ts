64d9206d5d90f220d178262f507dd425cf390bd4
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-30 19:53:36 +0200

    fix

--- src/app/shared/composables/useAppVersion.ts
+++ src/app/shared/composables/useAppVersion.ts
@@ -2,7 +2,7 @@ import { Deploy } from 'cordova-plugin-ionic'
 import { App } from '@capacitor/app'
 
 export async function useAppVersion() {
-  let appInfo = { version: 'unknown', build: 'unknown' }
+  let appInfo = { version: '??', build: '??' }
   try {
     appInfo = await App.getInfo()
   } catch (e) {
@@ -10,11 +10,11 @@ export async function useAppVersion() {
   }
   const deployInfo = await Deploy.getCurrentVersion()
 
-  const binaryVersion = deployInfo?.binaryVersionName || appInfo.version || 'unknown'
-  const binaryBuild = deployInfo?.binaryVersionCode || appInfo.build || 'unknown'
-  const version = deployInfo?.versionId ?? 'unknown'
-  const build = deployInfo?.buildId ?? 'unknown'
-  const channel = deployInfo?.channel ?? 'unknown'
+  const binaryVersion = deployInfo?.binaryVersionName || appInfo.version || '??'
+  const binaryBuild = deployInfo?.binaryVersionCode || appInfo.build || '??'
+  const version = deployInfo?.versionId ?? '??'
+  const build = deployInfo?.buildId ?? '??'
+  const channel = deployInfo?.channel ?? '??'
 
   return `${binaryVersion}v${binaryBuild} :: ${version}v${build} :: ${channel}`
 }
