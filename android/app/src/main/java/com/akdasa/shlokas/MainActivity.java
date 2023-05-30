fba5588d749ba3b3261881992432a5f8c2a77e9c
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-30 21:16:14 +0200

    test

--- android/app/src/main/java/com/akdasa/shlokas/MainActivity.java
+++ android/app/src/main/java/com/akdasa/shlokas/MainActivity.java
@@ -1,14 +1,5 @@
 package com.akdasa.shlokas;
 
-import android.os.Bundle;
-
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.registerPlugin(GoogleAuth.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
