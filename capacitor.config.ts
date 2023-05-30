76f975aa11c412d84d9aa59a9b1cbdaf9bb16d40
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-30 16:15:21 +0200

    chore(sql): cordova → capacitor (#236)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,6 +13,21 @@ const config: CapacitorConfig = {
       // We use WEB client ID here. Don't ask me why...
       'androidClientId': '879891505939-bs3v8sm9mu1ti65eu81f3b8idtl8bakm.apps.googleusercontent.com',
       'forceCodeForRefreshToken': true
+    },
+    'CapacitorSQLite': {
+      'iosDatabaseLocation': 'Library/CapacitorDatabase',
+      'iosIsEncryption': false,
+      'iosKeychainPrefix': 'cap',
+      'iosBiometric': {
+        'biometricAuth': false,
+        'biometricTitle' : 'Biometric login for capacitor sqlite'
+      },
+      'androidIsEncryption': false,
+      'androidBiometric': {
+        'biometricAuth' : false,
+        'biometricTitle' : 'Biometric login for capacitor sqlite',
+        'biometricSubTitle' : 'Log in using your biometric'
+      },
     }
   }
 }
