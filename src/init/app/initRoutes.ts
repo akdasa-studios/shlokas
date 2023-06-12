361d8a98e96afc371ea0ae3aa5e7c4067bd15ac1
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-06-12 14:12:01 +0200

    dont lock portrait orientation

--- src/init/app/initRoutes.ts
+++ src/init/app/initRoutes.ts
@@ -1,32 +1,17 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 
 import routes from '@/router'
-import { useScreenOrientation } from '@/app/shared/composables/useScreenOrientation'
 import { InitArgs } from '../initialization'
 
 
 export async function initRoutes(
   { get }: InitArgs
 ) {
-
-  const screenOrientation = useScreenOrientation()
   const vue = get('vue') as any
   const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes
   })
-
-
-  router.beforeEach(async guard => {
-    const unlockScreenOrientations = guard.meta?.unlockScreenOrientations
-
-    if (unlockScreenOrientations === true) {
-      await screenOrientation.unlock()
-    } else {
-      await screenOrientation.lock('portrait')
-    }
-  })
-
 
   vue.use(router)
   return { router }
