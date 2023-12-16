07bb94af66983b1f910a6c0a52e6d122bb9890f5
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-12-16 12:49:01 +0000

    Refactor canUserSwipeCardsUpInInboxDeck function

--- src/app/tutorial/guards/inboxDeck.ts
+++ src/app/tutorial/guards/inboxDeck.ts
@@ -1,14 +1,14 @@
 import { Direction } from '@/app/decks/shared'
 import { TutorialSteps, useTutorialStore } from '@/app/tutorial'
 
-const tutorial = useTutorialStore()
-
 /**
  * A user is not allowed to swipe cards up until he is asked for it.
  */
 export function canUserSwipeCardsUpInInboxDeck(
   direction: Direction
 ) {
+  const tutorial = useTutorialStore()
+
   if (!tutorial.inProgress) { return true }
 
   const isUserWasAskedToSwipeCardsUp = tutorial.atStep(TutorialSteps.InboxDeckSwipeCardUp)
